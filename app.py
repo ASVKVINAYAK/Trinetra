1005ff8ae8b8d1410528cf7f5e995ea5ba6d1c7f
TheSpeedX <ggspeedx29@gmail.com>
2021-03-31 03:22:35 +0530

    log feature update

--- app.py
+++ app.py
@@ -270,6 +270,8 @@ class ProfileView(Resource):
             # if (timediff < datetime.timedelta(hours=21)):
 
             prev_logs = user["current"].get("logs", [])
+            print("PREV: ", prev_logs)
+            print("LOGS: ", len(prev_logs))
             if len(prev_logs) <= 5:
                 users.update_one(
                     {'phone': resp['id']},
